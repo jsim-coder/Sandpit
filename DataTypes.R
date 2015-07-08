1dca536f36882bcfc3da169999167ea97bfe01ae
jsim-coder <James.Renouf@gmail.com>
2015-07-08 20:58:45 +1000

    Updated with new R file

---
+++ DataTypes.R
@@ -0,0 +1,22 @@
+
+## My first test file in R
+
+
+## Notes 
+## when you source on save in R studio it seems to execute the script
+## source('~/Code/R/Sandpit/DataTypes.R')
+
+x <- 1:20
+x # this does not seem to work when run in a script?
+print(x)
+print(y)
+
+x <- c(0.5,4,59)    ## Numeric
+print(x)
+
+y <- c("a:z")     ## Character
+print(y)
+
+
+
+  
